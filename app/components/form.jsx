a92d719e2d9d837309ddd66480852a520250e7ba
Zerosha <soldatumora@gmail.com>
2022-09-14 17:06:12 +0300

    global components refactor

--- app/components/form.jsx
+++ app/components/form.jsx
@@ -1,35 +1,43 @@
 const React = require("react");
 const Task = require("./task.jsx");
-const Storage = require("./storage.jsx");
+const Storage = require("./storage.js");
 
+const store = new Storage([], 0);
+//юайди 4 библиотеку
+//импорты вместо реквайр
+//добавить линтер(ESlint) и притиер 
 class Form extends React.Component {
   constructor(props) {
     super(props);
-    this.clickAddBtn = this.clickAddBtn.bind(this);
-    this.textBoxOnChange = this.textBoxOnChange.bind(this);
     this.state = {
       list: [],
       content: "",
       keyCounter: 0,
-      storage: new Storage([], 0),
     };
-    this.loadNotates(this.state.storage);
   }
-  clickAddBtn(event) {
-    if (this.state.content !== "") {
-      let mas = this.state.list;
-      let key = this.state.keyCounter + 1;
-      this.state.storage.add(key,this.state.content,false);
-      let newTask = <Task key={key} id={key} content={this.state.content} completed={false} storage={this.state.storage}/>;
-      mas.push(newTask);
-      this.setState({ list: mas, keyCounter: key });
+  handleRefresh = () => {
+    this.setState({
+      list: store.storage,
+      keyCounter:
+        store.storage.length > 0
+          ? store.storage[store.storage.length - 1].id
+          : 0,
+    });
+  };
+  handleClickAddBtn = (event) => {
+    if (event.target.previousSibling.value !== "") {
+      store.add(
+        this.state.keyCounter + 1,
+        event.target.previousSibling.value,
+        false
+      );
+      this.handleRefresh();
       event.target.previousSibling.value = "";
-      this.setState({ content: "" });
     } else {
       alert("In textBox mast be some information");
     }
-  }
-  textBoxOnChange(event) {
+  };
+  handleKeyUp = (event) => {
     if (event.key === "Enter") {
       const e = new Event("click", { bubbles: true });
       event.target.nextSibling.dispatchEvent(e);
@@ -38,39 +46,33 @@ class Form extends React.Component {
       event.target.value = "";
       event.target.blur();
     }
-    this.setState({ content: event.target.value });
-  }
-  async loadNotates(storage) {
-    let posts;
-    let url = "http://localhost:3000";
-    await fetch(`${url}/getposts`)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        posts = data;
-        console.log(data);
-      });
-
-    posts.forEach((elem) => {
-      storage.load(elem.id, elem.content, elem.completed);
-      let mas = this.state.list;
-      let newTask = <Task key={elem.id} id={elem.id} content={elem.content} completed={elem.completed} storage={this.state.storage}/>;
-      mas.push(newTask);
-      this.setState({ list: mas, keyCounter: elem.id });
-    });
+  };
+  async componentDidMount() {
+    await store.load();
+    this.handleRefresh();
   }
   render() {
-    this.state.storage.log();
+    //this.state.storage.log();
     return (
       <div>
         <input
           type="text"
           placeholder="Input something"
-          onKeyUp={this.textBoxOnChange}
+          onKeyUp={this.handleKeyUp}
         />
-        <button onClick={this.clickAddBtn}>Add</button>
-        <ul>{this.state.list}</ul>
+        <button onClick={this.handleClickAddBtn}>Add</button>
+        <ul>
+          {this.state.list.map((elem) => (
+            <Task
+              key={elem.id}
+              id={elem.id}
+              content={elem.content}
+              completed={elem.completed}
+              storage={store}
+              refresh={this.handleRefresh}
+            />
+          ))}
+        </ul>
       </div>
     );
   }
--- app/components/task.jsx
+++ app/components/task.jsx
@@ -1,86 +1,62 @@
 const React = require("react");
+
+
 class Task extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { completed: "content"  };
-    this.doneOnClick = this.doneOnClick.bind(this);
-    this.editOnClick = this.editOnClick.bind(this);
-    this.contentOnClick = this.contentOnClick.bind(this);
-    this. deleteOnClic=this. deleteOnClic.bind(this);
+    this.state = { nowOnChange: false, content: ""};
   }
-  deleteOnClic(event) {
-    event.target.parentNode.remove();
+  deleteOnClic = () => {
     this.props.storage.delete(this.props.id);
-  }
-  doneOnClick(event) {
-    if (event.target.textContent === "Uncomplete") {
-      event.target.textContent = "Complete";
-      event.target.nextSibling.nextSibling.disabled = false;
-    } else {
-      event.target.textContent = "Uncomplete";
-      event.target.nextSibling.nextSibling.disabled = true;
-    }
-    event.target.nextSibling.classList.toggle("done");
+    this.props.refresh();
+  };
+  doneOnClick = () => {
     this.props.storage.complete(this.props.id);
-  }
-  editOnClick(e) {
-    if (e.target.previousSibling.classList.contains("content")) {
-      const redBox = document.createElement("input");
-      redBox.value = e.target.previousSibling.textContent;
-      e.target.previousSibling.replaceWith(redBox);
-      let memory = redBox.value;
-      const btn = e.target;
-      e.target.previousSibling.addEventListener("keydown", (e) => {
-        if (e.key === "Enter") {
-          const event = new Event("click", { bubbles: true });
-          btn.dispatchEvent(event);
-        }
-        if (e.key === "Escape") {
-          e.target.value = memory;
-          e.target.blur();
-          const event = new Event("click", { bubbles: true });
-          btn.dispatchEvent(event);
-        }
-      });
-      e.target.textContent = "Save changes";
-      e.target.previousSibling.previousSibling.disabled = true;
-      e.target.nextSibling.disabled = true;
-    } else {
-      e.target.previousSibling.value = e.target.previousSibling.value.trim();
-      if (e.target.previousSibling.value.length != 0) {
-        const redContent = document.createElement("div");
-        redContent.classList.add("content");
-        redContent.textContent = e.target.previousSibling.value;
-        this.props.storage.edit(this.props.id, redContent.textContent)
-        redContent.addEventListener("click", () => {
-          const event = new Event("click", { bubbles: true });
-          e.target.dispatchEvent(event);
-        });
-        e.target.previousSibling.replaceWith(redContent);
-        e.target.textContent = "Edit";
-        e.target.previousSibling.previousSibling.disabled = false;
-        e.target.nextSibling.disabled = false;
-      } else {
-        alert("Input something");
-      }
+    this.props.refresh();
+  };
+  editOnClick = () => {
+    if (this.state.nowOnChange) {
+      this.props.storage.edit(this.props.id, this.state.content)};
+    this.setState({ nowOnChange: !this.state.nowOnChange });
+    this.props.refresh();
+  };
+  handleOnKeyUp = (e) => {
+    this.setState({ content: e.target.value });
+    if (e.key === "Enter") {
+      this.editOnClick();
     }
-  }
-  contentOnClick(e) {
-    const event = new Event("click", { bubbles: true });
-    e.target.nextSibling.dispatchEvent(event);
-  }
+    if (e.key === "Escape") {
+      //проблема! создать для инпука или дива отдельный компонент 
+      this.editOnClick();
+    }
+  };
+
   render() {
-    if (this.props.completed === true) {
-      this.setState({ completed: "content done" });
-    }
     return (
       <li>
-        <button onClick={this.doneOnClick} className="doneBtn">{this.props.completed === true?"Uncomplete":"Complete"}
+        <button onClick={this.doneOnClick} className="doneBtn">
+          {this.props.completed === true ? "Uncomplete" : "Complete"}
         </button>
-        <div className={this.state.completed} onClick={this.contentOnClick}>
-          {this.props.content}
-        </div>
-        <button onClick={this.editOnClick} disabled={this.props.completed}>Edit</button>
+        //таск тайтл отдельный компонент
+        {!this.state.nowOnChange ? (
+          <div
+            className={
+              this.props.completed === true ? "content done" : "content "
+            }
+            onClick={this.editOnClick}
+          >
+            {this.props.content}
+          </div>
+        ) : (
+          <input
+            type="text"
+            defaultValue={this.props.content}
+            onKeyUp={this.handleOnKeyUp}
+          ></input>
+        )}
+        <button onClick={this.editOnClick} disabled={this.props.completed}>
+          {!this.state.nowOnChange ? "Edit" : "Save"}
+        </button>
         <button onClick={this.deleteOnClic}>Delete</button>
       </li>
     );
